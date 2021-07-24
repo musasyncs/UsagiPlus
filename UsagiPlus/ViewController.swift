14aabb86c453cb943b57f38cf6f271571233d17b
musasyncs <rubato.cw@gmail.com>
2021-07-25 04:49:57 +0800

    Initial Commit

---
+++ UsagiPlus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsagiPlus
+//
+//  Created by Ewen on 2021/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
